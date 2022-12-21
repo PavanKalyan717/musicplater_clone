d0482c6fdef544b607dac2f17737f32b2f437d9d
Pavan Kalyan <pavankalyanm23@gmail.com>
2022-12-21 18:23:50 +0530

    project added

---
+++ src/pages/ArtistDetails.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+import { data } from 'autoprefixer';
+
+const ArtistDetails = () => {
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
+  console.log(artistData?.data[0]?.views['top-songs']?.data)
+  const topSongsData = artistData?.data[0]?.views['top-songs']?.data
+
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData}
+      />
+
+      <RelatedSongs
+        title = 'Top Songs'
+        data={topSongsData}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
+
+export default ArtistDetails;
